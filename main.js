2d095feb36cb84275ac9ddc843efa37e8b01f231
PublicoOne <120681199+PublicoOne@users.noreply.github.com>
2024-11-14 22:48:49 -0600

    v23

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-// Inicializar el mapa centrado en una ubicación de ejemplo
-const map = L.map('map').setView([25.7617, -80.1918], 5);
+import { createMapNormal } from './mapUtils/Create.js';
 
-// Añadir capa de mapa base de OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>'
-}).addTo(map);
 
-// Cargar el archivo GeoJSON y añadirlo al mapa
-fetch('trayectoria.geojson')
-    .then(response => response.json())
-    .then(data => {
-        // Añadir la capa GeoJSON al mapa
-        const geoLayer = L.geoJSON(data, {
-            style: feature => {
-                // Definir estilos según el tipo de geometría
-                if (feature.properties.type === 'trayectoria') {
-                    return { color: 'blue', weight: 3, opacity: 0.7 };
-                } else if (feature.properties.type === 'cono') {
-                    return { color: 'orange', fillColor: '#ff9f00', fillOpacity: 0.3 };
-                }
-            }
-        }).addTo(map);
-
-        // Ajustar la vista para encajar el contenido de la capa GeoJSON
-        map.fitBounds(geoLayer.getBounds());
-    })
-    .catch(error => console.error('Error cargando el GeoJSON:', error));
+createMapNormal();
---
+++ mapUtils/Create.js
@@ -0,0 +1,18 @@
+function createMapNormal(){
+    var map = L.map('map', {
+        center: [0, 0],
+        zoom: 2,
+        maxBounds: [
+            [-100, -Infinity], 
+            [100, Infinity]    
+        ],
+        maxBoundsViscosity: 0  
+    });
+    
+    
+    var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        minZoom: 2,
+        maxZoom: 22,
+    }).addTo(map);
+}
